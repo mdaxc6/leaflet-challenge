f9198f4163888179c605865be2dab9d4e4fb6504
mdaxc6 <mikeabsher1128@gmail.com>
2021-05-31 15:32:18 -0500

    added color scale and legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -17,16 +17,29 @@ function createFeatures(earthquakeData) {
         layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude: ${feature.properties.mag}<br>Date: ${new Date(feature.properties.time)}`);
 
     };
+    console.log(d3.extent(earthquakeData, d => d.geometry.coordinates[2]))
 
+    function chooseColor(mag){
+        return mag > 90 ? '#FF0D0D' :
+        mag > 70  ? '#FF4E11' :
+        mag > 50  ? '#FF8E15' :
+        mag > 30  ? '#FAB733' :
+        mag > 10  ? '#ACB334' :
+        mag > -10 ? '#69B34C' :
+                    '#006B3E';
+    }
 
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
+    var radiusScale = d3.scaleLinear()
+        .domain(d3.extent(earthquakeData, d => d.properties.mag))
+        .range([1,15])
+
+    
     var earthquakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature,
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, {
-                radius: feature.properties.mag * 3 ,
-                fillColor: "#ff7800",
+                radius: radiusScale(feature.properties.mag),
+                fillColor: chooseColor(feature.geometry.coordinates[2]),
                 color: "#000",
                 weight: 1,
                 opacity: 1,
@@ -35,11 +48,31 @@ function createFeatures(earthquakeData) {
         }
     });
 
+    var legend = L.control({ position: 'bottomright' })
+    legend.onAdd = function (map) {
+      var div = L.DomUtil.create('div', 'info legend')
+      var limits = [-10, 10, 30, 50, 70, 90]
+      var colors = ["#FFEDA0",'#FEB24C','#FD8D3C','#FC4E2A','#E31A1C','#BD0026','#800026']
+      var labels = []
+  
+      // Add min & max
+      div.innerHTML = '<div class="labels"><div class="min">' + limits[0] + '</div> \
+              <div class="max">' + limits[limits.length - 1] + '</div></div>'
+  
+      limits.forEach(function (limit, index) {
+        labels.push('<li style="background-color: ' + chooseColor(limits[index]) + '"></li>')
+      })
+  
+      div.innerHTML += '<ul>' + labels.join('') + '</ul>'
+      return div
+    }
+
+    
     // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes);
+    createMap(earthquakes, legend);
 }
 
-function createMap(earthquakes) {
+function createMap(earthquakes, legend) {
 
     // Define streetmap and darkmap layers
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -83,6 +116,9 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
+    
+    //add legend to map
+    legend.addTo(myMap);
 };
 
 window.addEventListener('DOMContentLoaded', init);
